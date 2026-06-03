6916a86f2e49965b26ab641c0444f99bde53521d
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-03 09:52:33 +0200

    Add task

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -2,5 +2,5 @@ INSERT INTO books (id, title, author, genre, price, stock, published_year) VALUE
 (1, 'The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 10.99, 5, 1937),
 (2, '1984', 'George Orwell', 'Dystopian', 8.99, 12, 1949),
 (3, 'Clean Code', 'Robert C. Martin', 'Tech', 32.50, 7, 2008),
-(4, 'The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 40, 1999),
-(5, 'Dune', 'Franck Herbert', 'Sci-Fi', 9.50, 9, 1965);
+(4, 'The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 4, 1999),
+(5, 'Dune', 'Frank Herbert', 'Sci-Fi', 9.50, 9, 1965);
