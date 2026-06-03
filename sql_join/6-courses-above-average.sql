2a32042490ea3a2a51bce5a0f1882d8614b16a82
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-03 14:24:13 +0200

    Add task

--- sql_join/6-courses-above-average.sql
+++ sql_join/6-courses-above-average.sql
@@ -3,8 +3,11 @@ FROM courses
 LEFT JOIN enrollments ON courses.id = enrollments.course_id
 GROUP BY courses.id, title
 HAVING COUNT(enrollments.student_id) > (
-    SELECT COUNT(*) FROM enrollments
-) * 1.0 / (
-    SELECT COUNT(*) FROM courses
+    SELECT AVG(total_students)
+    FROM (
+        SELECT COUNT(student_id) AS total_students
+        FROM enrollments
+        GROUP BY course_id
+    ) AS subquery
 )
 ORDER BY course_title;
