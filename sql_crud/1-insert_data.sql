6608ddc29fd853a877fe53e843a8595841206d9a
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-03 09:42:49 +0200

    Add task

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO books (id, title, author, genre, price, stock, published_year) VALUES
-(1, 'Le Hobbit', 'J.R.R. Tolkien', 'Fantaisie', 10.99, 5, 1937),
-(2, '1984', 'Georges Orwell', 'Dystopique', 8.99, 12, 1949),
-(3, 'Code propre', 'Robert C. Martin', 'Technologie', 32.50, 7, 2008),
-(4, 'Le programmeur pragmatique', 'André Chasse', 'Technologie', 28.75, 40, 1999),
-(5, 'Dunes', 'Franck Herbert', 'Science-fiction', 9.50, 9, 1965);
+(1, 'The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 10.99, 5, 1937),
+(2, '1984', 'Georges Orwell', 'Dystopian', 8.99, 12, 1949),
+(3, 'Clean Code', 'Robert C. Martin', 'Tech', 32.50, 7, 2008),
+(4, 'The Pragmatic Programmer', 'André Chasse', 'Tech', 28.75, 40, 1999),
+(5, 'Dune', 'Franck Herbert', 'Sci-Fi', 9.50, 9, 1965);
