c91895a18ca4ba7bbb60b150f61ff6e01266bf4c
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-04 13:59:13 +0200

    Add task

--- sql_join/8-courses-and-instructors.sql
+++ sql_join/8-courses-and-instructors.sql
@@ -1,4 +1,4 @@
-SELECT courses.title AS course_title, instructors.name AS instructor_name
+SELECT courses.title, instructors.name
 FROM courses
 INNER JOIN instructors ON courses.instructor_id = instructors.id
 ORDER BY course_title ASC;
