ee728c7f0a1533f21c76f5e921499d9cbdded858
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-04 15:26:01 +0200

    Add task

--- sql_join/13-courses-above-average-assignments.sql
+++ sql_join/13-courses-above-average-assignments.sql
@@ -3,6 +3,8 @@ FROM courses
 INNER JOIN assignments ON courses.id = assignments.course_id
 GROUP BY courses.id, courses.title
 HAVING COUNT(assignments.id) > (
-    (SELECT COUNT(id) FROM assignments) * 1.0 / (SELECT COUNT(id) FROM courses)
+    SELECT COUNT(*) FROM assignments
+) * 1.0 / (
+    SELECT COUNT(*) FROM courses
 )
 ORDER BY courses.title ASC;
