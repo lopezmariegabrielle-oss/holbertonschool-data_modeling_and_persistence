165929e62ead9470e28d0aea02ef1a2f192baaef
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-04 15:22:16 +0200

    Add task

--- sql_join/15-instructors-with-active-courses.sql
+++ sql_join/15-instructors-with-active-courses.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT instructors.name
 FROM instructors
 LEFT JOIN courses ON instructors.id = courses.instructor_id
-LEFT JOIN registractions ON courses.id = registrations.course_id
+LEFT JOIN registrations ON courses.id = registrations.course_id
 ORDER BY instructors.name ASC;
